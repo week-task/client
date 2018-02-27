bb319a14893400101e57f588f6ef53dd52518934
luolinjia <360512239@qq.com>
2018-02-27 10:01:21 +0800

    初始化路由

--- src/router/index.js
+++ src/router/index.js
@@ -9,7 +9,12 @@ export default new Router({
     {
       path: '/',
       name: 'HelloWorld',
-      component: HelloWorld
+      component: resolve => require(['@/components/HelloWorld'], resolve)
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: resolve => require(['@/components/Login'], resolve)
     }
   ]
 })
